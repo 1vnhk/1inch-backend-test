3dbfe1e8f425643fd0a2e61007de522d7c3c8990
Ivan Hrekov <iandrc9@gmail.com>
2026-02-06 12:21:49 +0100

    feat: Enable compression

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import compress from '@fastify/compress';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule, 
-    new FastifyAdapter()
+    AppModule,
+    new FastifyAdapter(),
   );
+
+  await app.register(compress, {
+    encodings: ['br', 'gzip', 'deflate'],
+    threshold: 1024,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
